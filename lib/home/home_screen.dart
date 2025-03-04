f68ba90f99591a54d3222b1cbe7d2db28b9e1d46
TheMedo <dimoski.ivan@gmail.com>
2025-03-04 09:52:27 +0100

    Write todo to Firebase

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,8 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +32,36 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Text(FirebaseAuth.instance.currentUser?.email ?? 'Not signed in'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                keyboardType: TextInputType.text,
+                controller: _controller,
+                decoration: const InputDecoration(
+                  labelText: 'Enter Task:',
+                ),
+              ),
+            ),
+            const SizedBox(width: 8),
+            ElevatedButton(
+              onPressed: () async {
+                final user = FirebaseAuth.instance.currentUser;
+                if (user != null && _controller.text.isNotEmpty) {
+                  await FirebaseFirestore.instance.collection('todos').add({
+                    'text': _controller.text,
+                    'createdAt': FieldValue.serverTimestamp(),
+                    'uid': user.uid,
+                  });
+                  _controller.clear();
+                }
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        ),
       ),
     );
   }
